a70c0c35a3beecc537b3386b132cc7a995f316c9
DeepanshuGOYAL05 <500052753@stu.upes.ac.in>
2020-10-07 11:25:34 +0530

    changes done

--- mysql database/schooldata.sql
+++ mysql database/schooldata.sql
@@ -3,11 +3,9 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Sep 29, 2020 at 08:08 PM
+-- Generation Time: Oct 07, 2020 at 07:53 AM
 -- Server version: 10.4.13-MariaDB
 -- PHP Version: 7.2.32
-
-
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -39,7 +37,8 @@ CREATE TABLE `abatches` (
 
 INSERT INTO `abatches` (`name`) VALUES
 ('btech16'),
-('btech17');
+('btech17'),
+('btech18');
 
 -- --------------------------------------------------------
 
@@ -96,19 +95,6 @@ CREATE TABLE `cnroll` (
   `batch` varchar(200) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
---
--- Dumping data for table `cnroll`
---
-
-INSERT INTO `cnroll` (`cid`, `cname`, `tid`, `tname`, `batch`) VALUES
-(4, 'Artificial Intelligence', 1, 0, 'btech16'),
-(6, 'Microprocessor', 1, 0, 'btech16'),
-(7, 'Discrete Math', 1, 0, 'btech16'),
-(9, 'Software Engineering', 1, 0, 'btech16'),
-(10, 'Algorithms', 1, 0, 'btech16'),
-(11, 'Cryptography', 1, 0, 'btech16'),
-(12, 'Graphics', 6, 0, 'btech16');
-
 -- --------------------------------------------------------
 
 --
@@ -122,19 +108,6 @@ CREATE TABLE `course` (
   `tname` varchar(2000) DEFAULT NULL,
   `nclass` date DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-
---
--- Dumping data for table `course`
---
-
-INSERT INTO `course` (`id`, `cname`, `tid`, `tname`, `nclass`) VALUES
-(4, 'Artificial Intelligence', 1, 'Gilbert	Hanson', '0000-00-00'),
-(6, 'Microprocessor', 1, 'Gilbert	Hanson', '2016-10-20'),
-(7, 'Discrete Math', 1, 'Gilbert	Hanson', '2016-10-20'),
-(9, 'Software Engineering', 1, 'Gilbert	Hanson', '2017-10-10'),
-(10, 'Algorithms', 1, 'Gilbert	Hanson', '2017-01-05'),
-(11, 'Cryptography', 1, 'Gilbert	Hanson', '2017-11-10'),
-(12, 'Graphics', 6, 'Argus Filch', '2020-10-05');
 
 -- --------------------------------------------------------
 
@@ -151,15 +124,6 @@ CREATE TABLE `cwork` (
   `url` varchar(1000) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
---
--- Dumping data for table `cwork`
---
-
-INSERT INTO `cwork` (`id`, `cid`, `tid`, `title`, `descr`, `url`) VALUES
-(1, 10, 1, 'Intro3', NULL, 'http://youtube.com/'),
-(2, 4, 1, 'Please follow the below tutorials', NULL, 'https://www.youtube.com/watch?v=4jmsHaJ7xEA&list=PL9ooVrP1hQOGHNaCT7_fwe9AabjZI1RjI'),
-(6, 12, 6, 'Click on this for Graphic Tutorial', NULL, 'https://www.youtube.com/watch?v=pz-lwONtVmM');
-
 -- --------------------------------------------------------
 
 --
@@ -174,33 +138,6 @@ CREATE TABLE `days` (
   `cid` int(11) DEFAULT NULL,
   `tid` int(11) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-
---
--- Dumping data for table `days`
---
-
-INSERT INTO `days` (`id`, `date`, `type`, `descr`, `cid`, `tid`) VALUES
-(2, '2016-10-02', 2, NULL, NULL, 1),
-(3, '2016-10-02', 2, NULL, NULL, 1),
-(4, '2016-10-02', 2, NULL, NULL, 1),
-(5, '2016-10-03', 1, NULL, 1, 1),
-(6, '2016-10-08', 1, NULL, 6, 1),
-(7, '2016-10-03', 1, NULL, 1, 1),
-(12, '2016-10-08', 3, NULL, 4, 1),
-(13, '2016-07-07', 2, NULL, NULL, 1),
-(14, '2016-11-03', 1, NULL, 1, 1),
-(15, '2016-07-07', 2, NULL, NULL, 1),
-(16, '2017-11-01', 2, NULL, NULL, 1),
-(17, '2017-11-01', 2, NULL, NULL, 1),
-(18, '2016-10-10', 4, NULL, NULL, 1),
-(19, '2017-01-10', 4, NULL, NULL, 1),
-(20, '2020-10-05', 3, NULL, 7, 1),
-(21, '2020-10-05', 3, NULL, 7, 1),
-(22, '2020-10-03', 4, NULL, NULL, 1),
-(24, '2020-10-07', 1, NULL, 12, 6),
-(25, '2020-10-20', 3, NULL, 12, 6),
-(26, '2020-10-02', 2, NULL, NULL, 6),
-(27, '2020-10-18', 4, NULL, NULL, 6);
 
 -- --------------------------------------------------------
 
@@ -217,35 +154,22 @@ CREATE TABLE `feed` (
   `batch` varchar(200) NOT NULL DEFAULT 'all'
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
+-- --------------------------------------------------------
+
 --
--- Dumping data for table `feed`
+-- Table structure for table `formkeys`
 --
 
-INSERT INTO `feed` (`tid`, `id`, `tname`, `title`, `exp_date`, `batch`) VALUES
-(1, 2, 'Gilbert	Hanson', '2016-07-07 has been declared a holiday!', '2016-07-07', 'all'),
-(1, 4, 'Gilbert	Hanson', '2017-11-01 has been declared a holiday!', '2017-11-01', 'all'),
-(1, 5, 'Gilbert Hanson', 'You have been added to a new course  by instructor Gilbert Hanson', '0000-00-00', ''),
-(1, 6, 'Gilbert Hanson', 'You have been added to a new course Algorithms and Data Structures by instructor Gilbert Hanson', '0000-00-00', 'btech16'),
-(1, 7, 'Gilbert Hanson', 'You have been added to a new course  by instructor Gilbert Hanson', '0000-00-00', ''),
-(1, 8, 'Gilbert Hanson', 'You have been added to a new course abcdef by instructor Gilbert Hanson', '0000-00-00', 'btech16'),
-(1, 9, 'Gilbert Hanson', 'You have been added to a new course  by instructor Gilbert Hanson', '2016-10-03', ''),
-(1, 10, 'Gilbert Hanson', 'You have been added to a new course Algorithms by instructor Gilbert Hanson', '2016-10-03', 'btech16'),
-(1, 11, 'Gilbert Hanson', 'You have been added to a new course Cryptography by instructor Gilbert Hanson', '2016-10-03', 'btech16'),
-(1, 12, 'Gilbert Hanson', 'There is parent teacher meet with Prof. Gilbert Hanson on 2016-10-10 ', '2016-10-10', 'all'),
-(1, 13, 'Gilbert	Hanson', '<span class=imp>There is parent teacher meet with Prof. Gilbert	Hanson on 2017-01-10 </span>', '2017-01-10', 'all'),
-(1, 14, 'Gilbert	Hanson', '<span id=http://google.com class=impp>Professor Gilbert	Hansonupdated the coursework on AI </span>', '0000-00-00', ''),
-(1, 15, 'Gilbert	Hanson', '<a href=http://google.com >Professor Gilbert	Hansonupdated the coursework on AI </a>', '2016-10-06', 'btech16'),
-(1, 16, 'Gilbert	Hanson', '<a href=http://youtube.com >Professor Gilbert	Hanson added DM intro to the coursework on Discreet Math </a>', '2016-10-06', 'btech16'),
-(1, 17, 'Gilbert	Hanson', '<a href=http://youtube.com/ >Professor Gilbert	Hanson added Intro3 to the coursework on Algorithms </a>', '2016-10-06', 'btech16'),
-(1, 18, 'Gilbert	Hanson', 'There will be an Mathematics Assignment exam of 100 marks on Gilbert	Hanson on 2020-10-05 ', '2020-10-05', 'all'),
-(1, 20, 'Gilbert	Hanson', '<a href=https://www.youtube.com/watch?v=4jmsHaJ7xEA&list=PL9ooVrP1hQOGHNaCT7_fwe9AabjZI1RjI >Professor Gilbert	Hanson added Please follow the below tutorials to the coursework on AI </a>', '2020-09-30', 'btech16'),
-(1, 24, 'Gilbert	Hanson', '<span class=imp>There is parent teacher meet with Prof. Gilbert	Hanson on 2020-10-03 </span>', '2020-10-03', 'all'),
-(6, 26, 'Argus Filch', 'You have been added to a new course Graphics by instructor Argus Filch', '2020-10-05', 'btech16'),
-(6, 27, 'Argus Filch', '<a href=https://www.youtube.com/watch?v=pz-lwONtVmM >Professor Argus Filch added Click on this for Graphic Tutorial to the coursework on Graphics </a>', '2020-10-04', 'btech16'),
-(6, 28, 'Argus Filch', 'There is a class by Prof. Argus Filch on 2020-10-07 ', '2020-10-07', 'btech16'),
-(6, 29, 'Argus Filch', 'There will be an Graphics Assignment exam of 100 marks on Argus Filch on 2020-10-20 ', '2020-10-20', 'all'),
-(6, 30, 'Argus Filch', '<span class=imp>2020-10-02 has been declared a holiday!</span>', '2020-10-02', 'all'),
-(6, 31, 'Argus Filch', '<span class=imp>There is parent teacher meet with Prof. Argus Filch on 2020-10-18 </span>', '2020-10-18', 'all');
+CREATE TABLE `formkeys` (
+  `teacher_key` varchar(25) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `formkeys`
+--
+
+INSERT INTO `formkeys` (`teacher_key`) VALUES
+('p@ssword');
 
 -- --------------------------------------------------------
 
@@ -280,14 +204,6 @@ CREATE TABLE `parent` (
   `pwd` varchar(2000) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
---
--- Dumping data for table `parent`
---
-
-INSERT INTO `parent` (`id`, `pname`, `sid`, `sname`, `email`, `pwd`) VALUES
-(1, 'Parent1', 1, 'Tameesh Biswas', 'parent1@parent.com', 'pswd'),
-(2, 'parent2', 2, 'Shaurya Gupta', 'agadga@sbafg.aha', 'pswd');
-
 -- --------------------------------------------------------
 
 --
@@ -313,7 +229,8 @@ INSERT INTO `stud` (`id`, `sname`, `points`, `email`, `pwd`, `batch`) VALUES
 (3, 'Saraansh Chopra', 0, 'saraansh@student.org', 'pswd', 'btech16'),
 (4, 'Deepanshu Goyal', 0, 'goyaldeepanshu0098@gmail.com', 'pswd', 'btech16'),
 (5, 'Deepanshu', 0, 'deepanshu05@gmail.com', 'psswd', 'btech16'),
-(6, 'Mukund', 0, 'mukund05@gmail.com', 'pswd', 'btech16');
+(6, 'Mukund', 0, 'mukund05@gmail.com', 'pswd', 'btech16'),
+(7, 'Deepanshu Goyal', 0, 'goyaldeepanshu0098@gmail.com', 'pswd', 'btech18');
 
 -- --------------------------------------------------------
 
@@ -377,15 +294,6 @@ CREATE TABLE `tests` (
   `batch` varchar(200) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
---
--- Dumping data for table `tests`
---
-
-INSERT INTO `tests` (`testid`, `name`, `cid`, `date`, `tid`, `maxmarks`, `batch`) VALUES
-(1, 'Midsems', 4, '2016-10-08', 1, 100, 'btech16'),
-(2, 'Mathematics Assignment', 7, '2020-10-05', 1, 100, 'btech16'),
-(4, 'Graphics Assignment', 12, '2020-10-20', 6, 100, 'btech17');
-
 -- --------------------------------------------------------
 
 --
@@ -404,6 +312,12 @@ CREATE TABLE `tscores` (
 --
 -- Indexes for dumped tables
 --
+
+--
+-- Indexes for table `abatches`
+--
+ALTER TABLE `abatches`
+  ADD UNIQUE KEY `name` (`name`);
 
 --
 -- Indexes for table `class`
@@ -492,25 +406,25 @@ ALTER TABLE `class`
 -- AUTO_INCREMENT for table `course`
 --
 ALTER TABLE `course`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=13;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
 
 --
 -- AUTO_INCREMENT for table `cwork`
 --
 ALTER TABLE `cwork`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
 
 --
 -- AUTO_INCREMENT for table `days`
 --
 ALTER TABLE `days`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=28;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
 
 --
 -- AUTO_INCREMENT for table `feed`
 --
 ALTER TABLE `feed`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=32;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
 
 --
 -- AUTO_INCREMENT for table `homework`
@@ -522,13 +436,13 @@ ALTER TABLE `homework`
 -- AUTO_INCREMENT for table `parent`
 --
 ALTER TABLE `parent`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
 
 --
 -- AUTO_INCREMENT for table `stud`
 --
 ALTER TABLE `stud`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
 
 --
 -- AUTO_INCREMENT for table `tblmessage`
@@ -546,16 +460,9 @@ ALTER TABLE `teachr`
 -- AUTO_INCREMENT for table `tests`
 --
 ALTER TABLE `tests`
-  MODIFY `testid` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+  MODIFY `testid` int(11) NOT NULL AUTO_INCREMENT;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
-
-/*Reset the autoincremented value, and then count every row while a new value is created for it.*/;
-SET @number := 0;
-UPDATE your_table SET id = @number := (@number+1);
-ALTER TABLE tableName AUTO_INCREMENT = 1;
-
-
